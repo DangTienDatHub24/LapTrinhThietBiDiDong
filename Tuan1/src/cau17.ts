c29340bcb43bb5ebc1aed5b7271ab9aa22750b91
H82M19\Student <Student>
2026-08-23 17:11:14 +0700

    Cau 17

---
+++ Tuan1/src/cau17.ts
@@ -0,0 +1,21 @@
+class Logger {
+    private static instance: Logger;
+
+    private constructor() {}
+
+    static getInstance(): Logger {
+        if (!Logger.instance) {
+            Logger.instance = new Logger();
+        }
+        return Logger.instance;
+    }
+
+    log(message: string): void {
+        console.log(message);
+    }
+}
+
+const logger = Logger.getInstance();
+
+logger.log("Hello World!");
+logger.log("This is a singleton logger.");
