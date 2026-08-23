6d1525b5b9f4a132b3814bcd3274f8a7f875578a
H82M19\Student <Student>
2026-08-23 16:27:08 +0700

    Cau 7

---
+++ Tuan1/src/cau7.ts
@@ -0,0 +1,20 @@
+class User{
+    private name: string;
+
+    constructor(name: string){
+        this.name = name;
+    }
+
+    getName(): string{
+        return this.name;
+    }
+
+    setName(name: string): void{
+        this.name = name;
+    }
+}
+
+const user = new User("Dat");
+console.log(`Name: ${user.getName()}`);
+user.setName("Dang Tien Dat");
+console.log(`Name: ${user.getName()}`);
