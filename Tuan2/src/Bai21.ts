bac6869d1fa6384a2342084ff8cfb123d7128aca
H82M19\Student <Student>
2026-09-06 17:05:28 +0700

    Bai 21

---
+++ Tuan2/src/Bai21.ts
@@ -0,0 +1,15 @@
+async function fetchTodo() {
+    const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    
+    const data = await response.json();
+    
+    console.log(data);
+    
+    
+    }
+    
+    fetchTodo();
+    
+    export {};
