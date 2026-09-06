506311200f8764d9fb34b69b12d9c416ce4edd8c
H82M19\Student <Student>
2026-09-06 16:26:43 +0700

    Bai 7

---
+++ Tuan2/src/Bai7.ts
@@ -0,0 +1,15 @@
+function simulateTask(time: number): Promise<string> {
+    return new Promise((resolve) => {
+    setTimeout(() => {
+    resolve(`Task done after ${time}ms`);
+    }, time);
+    });
+    }
+    
+    Promise.race([
+    simulateTask(3000),
+    simulateTask(1000),
+    simulateTask(2000)
+    ]).then((result) => {
+    console.log(result);
+    });
